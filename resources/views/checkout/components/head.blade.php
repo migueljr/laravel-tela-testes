6d27867f37aec52bc85daf2d663665bdc0360e80
Miguel Jr <miguel_jr@mail.com>
2022-01-13 10:19:02 -0300

    a

--- resources/views/checkout/components/head.blade.php
+++ resources/views/checkout/components/head.blade.php
@@ -20,7 +20,7 @@
     </div>
 
     <div id="bar-logo" style="display:flex;justify-content:space-between;">
-        <img width="160" height="43" style="margin-left:15px;" src="https://www.adoorei.com.br/wp-content/uploads/2021/08/logo-header.svg" alt="Adoorei">
+        <img width="160" height="43" src="https://www.adoorei.com.br/wp-content/uploads/2021/08/logo-header.svg" alt="Adoorei">
         
         <div 
             id="logo-sucurity-desktop"
@@ -57,7 +57,7 @@
                 <span>SITE 100% PROTEGIDO</span>
             </div>
         </div>
-        <div id="cart-mobile" style="margin-right:15px;" class="flex items-center">
+        <div id="cart-mobile" class="flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
             </svg>
@@ -85,6 +85,11 @@
 
         #cart-mobile{
             display:none;
+            margin-right:15px;
+        }
+
+        #bar-logo img{
+            margin-left:15px;
         }
 
         #cart-mobile{
@@ -101,7 +106,7 @@
             text-align:center;
             color:white;
         }
-        #bar-logo{margin-top:15px;width:1024px;}
+        #bar-logo{margin-top:15px;width:1024px;align-items:center;}
 
         #notify-cart{
             background-color:#F30168;
@@ -139,7 +144,8 @@
 
         @media (max-width: 575px)
         {
-            #cart-mobile{order:2;}
+            #cart-mobile{order:2; margin-right: 30px;}
+            #bar-logo img{margin-left:30px;}
             #logo-sucurity-desktop{order:1;}
             #cart-mobile{display:block;}
             #bar-security-mobile{display: flex;}
